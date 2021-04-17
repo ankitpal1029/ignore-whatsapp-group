ee68de3f590f4f25c613fc8d6969dbe21db5219b
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-04-17 20:57:15 +0530

    MINOR: minor ui fixes

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -4,6 +4,18 @@
     //MyEvent.initMouseEvent(ElementType, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); 
     //MyElement.dispatchEvent(MyEvent); 
 //}
+
+function getParentNode(element, level=1) { // 1 - default value (if no 'level' parameter is passed to the function)
+    while(level-- > 0) {
+        element = element.parentNode;
+        if(!element) {
+            return null; // to avoid a possible "TypeError: Cannot read property 'parentNode' of null" if the requested level is higher than document
+        }
+    }
+    return element;
+}
+
+
 inject = async()=>{ 
   console.log('helloo');
     let box= document.querySelectorAll("[dir='auto']"); 
@@ -22,7 +34,9 @@ inject = async()=>{
     if(box[i].innerHTML.includes(`${grpName}`)){
         console.log('found punday');
         console.log(box[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
-        box[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+        //box[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+        let element = getParentNode(box[i],6)
+        element.remove();
       }
     }
 
